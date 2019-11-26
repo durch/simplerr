998cac097ea764a6bfb4e57ffac07d82a5368039
Drazen Urch <drazen@urch.eu>
2019-11-26 11:47:39 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+// Creates and error for the current crate, aliases Result to use the new error and defines a from! macro to use for converting to
+// the local error type.
+#[macro_export]
+macro_rules! err {
+    ($i: ident) => {
+        #[derive(Debug)]
+        pub struct $i {
+            pub description: Option<String>,
+        }
+
+        impl From<&str> for $i {
+            fn from(str: &str) -> Self {
+                $i {
+                    description: Some(str.to_string()),
+                }
+            }
+        }
+
+        impl fmt::Display for $i {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                match self.description.as_ref() {
+                    Some(err) => write!(f, "{}", err),
+                    None => write!(f, "An unknown error has occurred!"),
+                }
+            }
+        }
+        pub type Result<T, E = $i> = std::result::Result<T, E>;
+
+        macro_rules! from {
+            ($t: ty) => {
+                impl From<$t> for $i {
+                    fn from(e: $t) -> $i {
+                        $i {
+                            description: Some(String::from(format!("{}", e))),
+                        }
+                    }
+                }
+            };
+        }
+    };
+}
